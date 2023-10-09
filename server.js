768047b429873d9f84f0a272353bfb08878d31f4
Danny <dgr73@cornell.edu>
2023-10-09 16:43:21 -0400

    added routing

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Route files
+const expenses = require("./routes/expenses");
+
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+app.use("/api/v1/expenses", expenses);
+
+app.get("/api/v1/expenses", (req, res) => {
+  res.status(200).json("hello from Express");
+});
 
 const PORT = process.env.PORT || 6000;
 
